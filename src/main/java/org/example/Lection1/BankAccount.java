06c0ae7b3586093930f876086d8d854966b08a02
SystemSL <Dmitriy.sulimov2003@gmail.com>
2025-06-24 23:23:02 +0400

    Additional tasks

--- src/main/java/org/example/Lection1/BankAccount.java
+++ src/main/java/org/example/Lection1/BankAccount.java
@@ -1,6 +1,8 @@
 package org.example.Lection1;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Random;
 
 import static java.time.LocalDateTime.now;
 
@@ -9,12 +11,16 @@ public class BankAccount {
     private int balance;
     private LocalDateTime openingData;
     private boolean blocked;
+    private String number;
+    private Random random;
 
     public BankAccount(String name) {
+        this.random = new Random();
         this.name = name;
         this.balance = 0;
         this.openingData = now();
         this.blocked = false;
+        this.number = String.format("%09d", random.nextInt((int) 1e10));
     }
 
     public boolean deposit(int amount) {
@@ -41,5 +47,16 @@ public class BankAccount {
         return this.withdraw(amount) && (otherAccount.deposit(amount) || !this.deposit(amount));
     }
 
+    public String toString() {
+        return "Name: " + this.name + "\nBalance: " + this.balance + "\nOpening date: " +
+                this.openingData.toLocalDate() + "\nBlocked: " + (this.blocked ? "yes" : "no") + "\nNumber: " + this.number;
+    }
 
+    public boolean equals(BankAccount otherAccount) {
+        return (Objects.equals(this.name, otherAccount.name) && this.balance == otherAccount.balance);
+    }
+
+    public int hashCode() {
+        return 31 * this.name.hashCode() + this.balance;
+    }
 }
--- src/main/java/org/example/Lection1/testBankAccount.java
+++ src/main/java/org/example/Lection1/testBankAccount.java
@@ -4,11 +4,17 @@ public class testBankAccount {
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount("Dmitriy");
         BankAccount account2 = new BankAccount("Sergey");
+        BankAccount account3 = new BankAccount("Dmitriy");
         System.out.println(account1.withdraw(100));
         System.out.println(account1.deposit(200));
         System.out.println(account1.withdraw(100));
         System.out.println(account1.transfer(account2, 100));
         System.out.println(account1.transfer(account2, 100));
         System.out.println(account2.withdraw(100));
+        System.out.println(account1);
+        System.out.println(account1.equals(account3));
+        System.out.println(account1.hashCode());
+        System.out.println(account2.hashCode());
+        System.out.println(account3.hashCode());
     }
 }
